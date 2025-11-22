f632d1dba3bf3032e74c1f48c0d0afb817c163df
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-11-22 15:55:21 +0500

    chore: install eslint

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+    rules: { 'no-console': 'error' },
+  },
+]);
