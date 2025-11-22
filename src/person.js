ca5cf9966274c5bb06b7f3fbd9dca144ee6208f0
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-11-22 15:02:08 +0500

    feat: add person.js task

---
+++ src/person.js
@@ -0,0 +1,84 @@
+import { throwNotImplementedError } from '../utils/throw-not-implemented-error.js';
+
+export class Person {
+  /**
+   * @public
+   * @static
+   * @type {number}
+   */
+  static count = 0;
+
+  /**
+   * @public
+   * @static
+   * @type {number}
+   */
+  static totalWeight = 0;
+
+  /**
+   * @public
+   * @static
+   * @type {number}
+   */
+  static totalHeight = 0;
+
+  /**
+   * @public
+   * @static
+   * @returns {number}
+   */
+  static averageWeight() {
+    throwNotImplementedError();
+  }
+
+  /**
+   * @public
+   * @static
+   * @returns {number}
+   */
+  static averageHeight() {
+    throwNotImplementedError();
+  }
+
+  /**
+   * @param {number} weight
+   * @param {number} height
+   */
+  constructor(weight, height) {
+    throwNotImplementedError();
+  }
+
+  /**
+   * @public
+   * @returns {number}
+   */
+  get weight() {
+    throwNotImplementedError();
+  }
+
+  /**
+   * @public
+   * @param {number} weight
+   * @returns {number}
+   */
+  set weight(weight) {
+    throwNotImplementedError();
+  }
+
+  /**
+   * @public
+   * @returns {number}
+   */
+  get height() {
+    throwNotImplementedError();
+  }
+
+  /**
+   * @public
+   * @param {number} height
+   * @returns {number}
+   */
+  set height(height) {
+    throwNotImplementedError();
+  }
+}
